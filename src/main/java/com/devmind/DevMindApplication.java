8eece79ef1db8ccc8b83a8a301bb9873a91728b9
wty <tywang.dev@outlook.com>
2026-06-24 00:44:43 +0800

    fix: disable generated security user

--- src/main/java/com/devmind/DevMindApplication.java
+++ src/main/java/com/devmind/DevMindApplication.java
@@ -2,10 +2,11 @@ package com.devmind;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan("com.devmind.module")
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class DevMindApplication {
 
     public static void main(String[] args) {
